cf74aaed2ff1e997bf1b4b67f923500b68b96216
Chan Nyein Kyaw <channyein.myr@gmail.com>
2021-05-26 21:09:14 +0630

    adding clarifai

--- src/App.js
+++ src/App.js
@@ -7,29 +7,45 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 const particlesOptions = {
-	particles: {
-		number: {
-			value: 80,
-			density: {
-				enable: true,
-				value_area: 800
-			}
-		}
-	}
-}
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log('click');
+  };
+
   render() {
     return (
       <div className="App">
-		<Particles className='particles'
-			params={particlesOptions}
-		/>
-      	<Navigation />
-      	<Logo />
-      	<Rank />
-      	<ImageLinkForm />
-      	{/*
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/*
       	<FaceRecognition />*/}
       </div>
     );
